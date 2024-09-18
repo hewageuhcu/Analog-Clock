7d10f2fdc3f7098fb08a837b4c53607bd6d35f88
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-18 17:08:53 +0900

    html added and node modules added

--- A-Clock/src/app/app.component.ts
+++ A-Clock/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WatchComponent } from "./watch/watch.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WatchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
