d30a87332b4b8fd59066b3d25db2ee3847e102d5
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-09-18 16:57:39 +0900

    new comonent added

---
+++ A-Clock/src/app/watch/watch.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-watch',
+  standalone: true,
+  imports: [],
+  templateUrl: './watch.component.html',
+  styleUrl: './watch.component.css'
+})
+export class WatchComponent {
+
+}
